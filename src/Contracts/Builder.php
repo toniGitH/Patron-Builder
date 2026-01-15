4ee7502ffe1f50aedb5f939152e2476f83e1594a
tonigith <91883442+toniGitH@users.noreply.github.com>
2026-01-15 20:54:22 +0100

    Añadido comentario explicativo en interface Builder

--- src/Contracts/Builder.php
+++ src/Contracts/Builder.php
@@ -2,7 +2,14 @@
 
 namespace App\Contracts;
 
-// Este el el Builder propiamente dicho. Es el constructor GENÉRICO
+/* Este el el Builder propiamente dicho. Es el constructor GENÉRICO.
+   En este ejemplo, representa los diferentes pasos que se tienen que ejecutar para construir determinados objetos
+   que comparten una misma estructura de creación (podría ser un ordenador, su manual de instrucciones, la factura
+   de su coste de fabricación, etc...).
+   Por tanto, un Builder no es necesariamente específico de un solo tipo concreto de producto, pero sí es obligatorio
+   que los objetos que construya compartan la misma estructura de creación declarada en esta interfaz. 
+*/
+
 interface Builder
 {
     // Método típico de un patrón Builder para resetear el estado
