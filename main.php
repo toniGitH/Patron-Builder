975185788bbec1c028e780892ed5d22a237d9469
tonigith <91883442+toniGitH@users.noreply.github.com>
2026-01-16 07:37:24 +0100

    Renombrando archivos, clases, variables, etc... al inglés

--- main.php
+++ main.php
@@ -1,29 +1,111 @@
 <?php
 
-require_once 'vendor/autoload.php';
+    require_once 'vendor/autoload.php';
 
-use App\Products\Ordenador\BuilderOrdenador;
-use App\Director\Director;
+    use App\Domain\Computer\ComputerBuilder;
+    use App\Director\Director;
 
-// Crear un builder
-$builder = new BuilderOrdenador();
+    // 1. FABRICACIÓN DE UN ORDENADOR FÍSICO (MacBook Pro)
 
-// Crear un director (opcional) y asignarle el builder
-$director = new Director($builder);
+    // Instanciamos el Builder específico que crea orednadores físicos (el ComputerBuilder)
+    $builder = new ComputerBuilder();
 
-// Fabricar un ordenador MacBook Pro
-$director->fabricarMacBookPro();
-$ordenadorMacBookPro = $builder->obtenerOrdenadorFabricado();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    $director = new Director($builder);
 
-// Fabricar un ordenador GamerPro
-$director->fabricarGamerPro();
-$ordenadorGamerPro = $builder->obtenerOrdenadorFabricado();
+    // Fabricamos un ordenador MacBook Pro
+    $director->macBookPro();
 
-// Fabricar un ordenador Economy
-$director->fabricarEconomy();
-$ordenadorEconomy = $builder->obtenerOrdenadorFabricado();
+    // Obtenemos el ordenador fabricado
+    $macBookProComputer = $builder->getComputer();
 
-// Mostrar los ordenadores fabricados
-var_dump($ordenadorMacBookPro);
-var_dump($ordenadorGamerPro);
-var_dump($ordenadorEconomy);
+    // 2. ELABORACIÓN DEL MANUAL DE USUARIO DE UN ORDENADOR FÍSICO (del MacBook Pro)
+    // Instanciamos el Builder específico que crea manuals de usuario (el ManualBuilder)
+    //$builder = new ManualBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    //$director = new Director($builder);
+    // Fabricamos un manual de usuario para el MacBook Pro
+    //$director->macBookPro();
+    // Obtenemos el manual de usuario fabricado
+    //$macBookProManual = $builder->getManual();
+    
+    // 3. EMISIÓN DE LA FACTURA DEL COSTE DE FABRICACIÓN DE UN ORDENADOR FÍSICO (del MacBook Pro)
+    // Instanciamos el Builder específico que crea facturas (el InvoiceBuilder)
+    //$builder = new InvoiceBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    //$director = new Director($builder);
+    // Fabricamos una factura para el MacBook Pro
+    //$director->macBookPro();
+    // Obtenemos la factura fabricada
+    //$macBookProInvoice = $builder->getInvoice();
+
+    // 4. FABRICACIÓN DE UN ORDENADOR FÍSICO (GamerPro)
+    // Instanciamos el Builder específico que crea orednadores físicos (el ComputerBuilder)
+    $builder = new ComputerBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    $director = new Director($builder);
+    // Fabricamos un ordenador GamerPro
+    $director->gamingPro();
+    // Obtenemos el ordenador fabricado
+    $gamingProComputer = $builder->getComputer();
+
+    // 5. ELABORACIÓN DEL MANUAL DE USUARIO DE UN ORDENADOR FÍSICO (del GamerPro)
+    // Instanciamos el Builder específico que crea manuals de usuario (el ManualBuilder)
+    //$builder = new ManualBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    //$director = new Director($builder);
+    // Fabricamos un manual de usuario para el GamerPro
+    //$director->gamingPro();
+    // Obtenemos el manual de usuario fabricado
+    //$gamingProManual = $builder->getManual();
+    
+    // 6. EMISIÓN DE LA FACTURA DEL COSTE DE FABRICACIÓN DE UN ORDENADOR FÍSICO (del GamerPro)
+    // Instanciamos el Builder específico que crea facturas (el InvoiceBuilder)
+    //$builder = new InvoiceBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    //$director = new Director($builder);
+    // Fabricamos una factura para el GamerPro
+    //$director->gamingPro();
+    // Obtenemos la factura fabricada
+    //$gamingProInvoice = $builder->getInvoice();
+
+    // 7. FABRICACIÓN DE UN ORDENADOR FÍSICO (Acer Economy)
+    // Instanciamos el Builder específico que crea orednadores físicos (el ComputerBuilder)
+    $builder = new ComputerBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    $director = new Director($builder);
+    // Fabricamos un ordenador Acer Economy
+    $director->acerEconomy();
+    // Obtenemos el ordenador fabricado
+    $acerEconomyComputer = $builder->getComputer();
+
+    // 8. ELABORACIÓN DEL MANUAL DE USUARIO DE UN ORDENADOR FÍSICO (del Acer Economy)
+    // Instanciamos el Builder específico que crea manuals de usuario (el ManualBuilder)
+    //$builder = new ManualBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    //$director = new Director($builder);
+    // Fabricamos un manual de usuario para el Acer Economy
+    //$director->acerEconomy();
+    // Obtenemos el manual de usuario fabricado
+    //$acerEconomyManual = $builder->getManual();
+    
+    // 9. EMISIÓN DE LA FACTURA DEL COSTE DE FABRICACIÓN DE UN ORDENADOR FÍSICO (del Acer Economy)
+    // Instanciamos el Builder específico que crea facturas (el InvoiceBuilder)
+    //$builder = new InvoiceBuilder();
+    // Creamos un director (opcional) y le asignamos el builder que necesitamos
+    //$director = new Director($builder);
+    // Fabricamos una factura para el Acer Economy
+    //$director->acerEconomy();
+    // Obtenemos la factura fabricada
+    //$acerEconomyInvoice = $builder->getInvoice();
+
+    // Mostrar los resultados: ordenadores, manuales y facturas
+    var_dump($macBookProComputer);
+    //var_dump($macBookProManual);
+    //var_dump($macBookProInvoice);
+    var_dump($gamingProComputer);
+    //var_dump($gamingProManual);
+    //var_dump($gamingProInvoice);
+    var_dump($acerEconomyComputer);
+    //var_dump($acerEconomyManual);
+    //var_dump($acerEconomyInvoice);
